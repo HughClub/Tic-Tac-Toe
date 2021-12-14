25c2f7df234e2b50407fe992602b39cef14ef171
HUGHNew <1728902978@qq.com>
2021-12-14 17:31:20 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Tic-Tac-Toe
- Tic-Tac-Toe with EasyX (C++)
+ Tic-Tac-Toe(井字棋) with EasyX (C++)
+
+
+
+两种实现
+
+-   EasyX点击事件
+-   控制台输入
+
+解题思路源自<https://github.com/liuxinyu95/unplugged/blob/master/appendix/answer/ans-zh-cn.tex>
+
+
+
+一个VS C++项目，应该只能在Windows上运行。
+
+
+
+逻辑目录结构
+
+-   include
+    -   T3module.h(对应实现在T3Component.cpp 懒得改名了)
+    -   TicTacToeBase.h 虚基类 实现了大部分公有操作，属性全为protected
+    -   ConsoleTicTacToe.h
+    -   TicTacToe.h
+    -   Tools.hpp
+-   src
+    -   T3Component.cpp
+    -   TicTacToeBase.cpp
+    -   ConsoleTicTacToe.cpp
+    -   TicTacToe.cpp
+-   main.cpp
+
+
+
--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "T3module.h"
 #include "TicTacToe.h"
 #include "ConsoleTicTacToe.h"
-void PutAlphaImage(int img_x, int img_y, IMAGE* pSrcImg);
 void Console() {
   ConsoleTicTacToe CT;
   CT.play();
@@ -46,51 +45,4 @@ int main() {
   UI();
   //UI_test();
   return 0;
-}
-void PutAlphaImage(int img_x, int img_y, IMAGE* pSrcImg) {
-  DWORD* dst = GetImageBuffer();
-  DWORD* src = GetImageBuffer(pSrcImg);
-  int src_width = pSrcImg->getwidth();
-  int src_height = pSrcImg->getheight();
-  int dst_width = getwidth();
-  int dst_height = getheight();
-
-  int im_width, im_height;
-  if (img_x + src_width > dst_width)
-    im_width = dst_width - img_x;
-  else
-    im_width = src_width;
-  if (img_y + src_height > dst_height)
-    im_height = dst_height - img_y;
-  else
-    im_height = src_height;
-
-  if (img_x < 0) {
-    src = src - img_x;
-    im_width = im_width + img_x;
-    img_x = 0;
-  }
-  if (img_y < 0) {
-    src = src - src_width * img_y;
-    im_height = im_height + img_y;
-    img_y = 0;
-  }
-  dst = dst + dst_width * img_y + img_x;
-
-  for (int i = 0; i < im_height; i++) {
-    for (int j = 0; j < im_width; j++) {
-      int src_r = ((src[j] & 0xff0000) >> 16);
-      int src_g = ((src[j] & 0xff00) >> 8);
-      int src_b = src[j] & 0xff;
-      int src_a = ((src[j] & 0xff000000) >> 24);
-      int dst_r = ((dst[j] & 0xff0000) >> 16);
-      int dst_g = ((dst[j] & 0xff00) >> 8);
-      int dst_b = dst[j] & 0xff;
-      dst[j] = ((src_r + dst_r * (255 - src_a) / 255) << 16) |
-               ((src_g + dst_g * (255 - src_a) / 255) << 8) |
-               (src_b + dst_b * (255 - src_a) / 255);
-    }
-    dst = dst + dst_width;
-    src = src + src_width;
-  }
 }
